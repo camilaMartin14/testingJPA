fccae21ad0e2f656cce29aa2c8a47e2df0d174cf
camilaMartin14 <camilamartin1403@gmail.com>
2024-06-19 12:06:47 -0300

    xdios

---
+++ src/main/java/com/mycompany/testingjpa/TestingJPA.java
@@ -0,0 +1,12 @@
+package com.mycompany.testingjpa;
+
+/**
+ *
+ * @author Camila
+ */
+public class TestingJPA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
