78a10b4f0bf06c3571d48c00cc341822c88fa7fa
camilaMartin14 <camilamartin1403@gmail.com>
2024-06-21 11:51:37 -0300

    agregar clase reps

--- src/main/java/gestorbd/lideres/lideres.java
+++ src/main/java/gestorbd/lideres/lideres.java
@@ -6,4 +6,54 @@ package gestorbd.lideres;
  */
 public class lideres {
     
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String fechaContratacion;
+
+    public lideres() {
+    }
+
+    public lideres(int id, String nombre, String apellido, String fechaContratacion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaContratacion = fechaContratacion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getFechaContratacion() {
+        return fechaContratacion;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setFechaContratacion(String fechaContratacion) {
+        this.fechaContratacion = fechaContratacion;
+    }
+    
+    
+       
+    
 }
---
+++ src/main/java/gestorbd/lideres/representantes.java
@@ -0,0 +1,68 @@
+package gestorbd.lideres;
+
+/**
+ *
+ * @author Camila
+ */
+public class representantes {
+    
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String fechaContratacion;
+    private double salario;
+
+    public representantes() {
+    }
+
+    public representantes(int id, String nombre, String apellido, String fechaContratacion, double salario) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaContratacion = fechaContratacion;
+        this.salario = salario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFechaContratacion() {
+        return fechaContratacion;
+    }
+
+    public void setFechaContratacion(String fechaContratacion) {
+        this.fechaContratacion = fechaContratacion;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+
+}
+
